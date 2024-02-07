22c46090958e4d6ff02fb405fa2850ca9047b5b1
/kaaz <92046563+KaazDW@users.noreply.github.com>
2024-02-07 15:31:17 +0100

    ProduitController /all

---
+++ README.md
@@ -0,0 +1,12 @@
+- ✅ En tant qu’utilisateur non connecté, je peux consulter la liste des magasins près de chez moi
+    `/magasins/all & /magasins/near dans MagasinController`
+- ✅ En tant qu’utilisateur non connecté, je peux consulter les produits vendus dans ces magasins
+    `/produits/all dans ProduitController`
+- En tant qu'utilisateur non connecté, je peux consulter si un produit est disponible en stock
+- En tant qu’utilisateur non connecté, je peux écrire un message à un vendeur
+- En tant qu'utilisateur non connecté, je peux m'inscrire afin de devenir un client
+- En tant que client, je peux commander un ou plusieurs produits dans un magasin
+- En tant que client, je dispose d'une liste de créneau disponible pour récupérer ma commande
+- En tant que client, je souhaite pouvoir réserver un créneau pour récupérer ma commande.
+- En tant qu’administrateur, je peux déclarer de nouvelles boutiques et mettre à jour le stock en ajoutant ou retirant des articles
+- En tant qu’administrateur, je peux notifier par mail mes clients que leur commande est validée.
--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -1,19 +1,38 @@
 <?php
 
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Produit;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ProduitController extends AbstractController
 {
-    #[Route('/produit', name: 'app_produit')]
-    public function index(): JsonResponse
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProduitController.php',
-        ]);
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/produits/all', name: 'produitsDisponibles', methods: ['GET'])]
+    public function produitsDisponibles(): JsonResponse
+    {
+        $produits = $this->entityManager->getRepository(Produit::class)->findAll();
+
+        $formattedProduits = [];
+        foreach ($produits as $produit) {
+            $formattedProduits[] = [
+                'id' => $produit->getId(),
+                'nom' => $produit->getNom(),
+                'description' => $produit->getDescription(),
+                'prix' => $produit->getPrix(),
+            ];
+        }
+
+        return $this->json($formattedProduits);
     }
 }
