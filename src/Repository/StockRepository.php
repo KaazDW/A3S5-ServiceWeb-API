aa1f5e47c5b812930610bb93fae05cd1274434b2
TRONTIN Pauline <morgane71480@gmail.com>
2024-02-07 17:39:05 +0100

    ajout stockrepository

--- src/Repository/StockRepository.php
+++ src/Repository/StockRepository.php
@@ -1,8 +1,22 @@
 <?php
 
-namespace App\Entity;
+namespace App\Repository;
 
-class StockRepository
+
+use App\Entity\Produit;
+use App\Entity\Stock;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Stock>
+ *
+ */
+class StockRepository extends ServiceEntityRepository
 {
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Stock::class);
+    }
 
 }
